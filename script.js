d62996e639b256be69003600d8904af94084de26
lacerda-akm <115191670+caramejunior@users.noreply.github.com>
2023-08-30 17:12:37 -0300

    criação de funções e estilização.

--- script.js
+++ script.js
@@ -36,6 +36,26 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-  // navbar
+  // button voltar ao topo.
+
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  // Exibir o botão quando a página for rolada para baixo
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+  
+  // Rolar suavemente para o topo quando o botão for clicado
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+  
 
 typeTextLine1();
